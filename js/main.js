29f28641167eb1666c59f7948dfd2961d73e4d47
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-02-27 21:42:52 +0700

    Web Sederhana

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
- const mntoggle = document.querySelector('.menu-toggle input');
- const nav = document.querySelector('nav ul');
+const firstNumber = document.getElementById('firstNumber');
+const secondNumber = document.getElementById('secondNumber');
+const result = document.getElementsByClassName('result')[0];
 
-mntoggle.addEventListener('click',function(){
-    nav.classList.toggle('menushow');
-})
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const divide = document.getElementById('divide');
+const multiple = document.getElementById('multiple');
+
+plus.addEventListener('click', ()=>{
+    let hasil = Number(firstNumber.value)+Number(secondNumber.value);
+    result.innerHTML = hasil;   
+});
+
+minus.addEventListener('click', ()=>{
+    let hasil = Number(firstNumber.value)-Number(secondNumber.value);
+    result.innerHTML = hasil;   
+});
+
+multiple.addEventListener('click', ()=>{
+    let hasil = Number(firstNumber.value)*Number(secondNumber.value);
+    result.innerHTML = hasil;   
+});
+
+divide.addEventListener('click', ()=>{
+    let hasil = Number(firstNumber.value)/Number(secondNumber.value);
+    result.innerHTML = hasil;   
+});
